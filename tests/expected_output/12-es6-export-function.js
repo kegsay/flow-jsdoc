797d91e97b20c86cec815e562faf11882cfc24be
John Wineman <jwineman@cloudflare.com>
2017-02-20 18:24:59 -0800

    Added support for export function foo(bar) {}.

---
+++ tests/expected_output/12-es6-export-function.js
@@ -0,0 +1,8 @@
+/**
+ * @param {Foobar[]} bar A foobar array
+ * @param {Function} baz
+ * @return {number}
+ */
+export function foo(bar: Array<Foobar>, baz: Function) : number {
+    return 42;
+}
